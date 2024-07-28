9c520424e43ad1804ad7a89e046b7977aa360ea8
usmanashraf <usman.ashraf@ext.careem.com>
2024-07-28 22:07:44 +0400

    refactored

--- hello_test.go
+++ hello_test.go
@@ -8,15 +8,26 @@ func TestHello(t *testing.T) {
 
 	t.Run("saying hello to people", func(t *testing.T) {
 		inputString := "Usman"
-		got := Hello(inputString)
+		got := Hello(inputString, "")
 
 		want := "Hello Usman"
 		assertCorrectMessage(t, got, want)
 
 	})
 	t.Run("print hello world when input string is empty", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello World"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in spanish", func(t *testing.T) {
+		got := Hello("Eldoie", "Spanish")
+		want := "Hola Eldoie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in frech", func(t *testing.T) {
+		got := Hello("Chris", "French")
+		want := "Bonjour Chris"
 		assertCorrectMessage(t, got, want)
 	})
 
--- main.go
+++ main.go
@@ -2,15 +2,34 @@ package main
 
 import "fmt"
 
-const namingPrefix = "Hello "
+const (
+	englishNamingPrefix = "Hello "
+	spanishNamingPrefix = "Hola "
+	frenchNamingPrefix  = "Bonjour "
+
+	spanish = "Spanish"
+	french  = "French"
+)
 
 func main() {
-	fmt.Println(Hello("Chris"))
+	fmt.Println(Hello("Chris", "English"))
 }
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if len(name) == 0 {
 		name = "World"
 	}
-	return namingPrefix + name
+	prefix := fetchPrefix(language)
+	return prefix + name
+}
+
+func fetchPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		return frenchNamingPrefix
+	case spanish:
+		return spanishNamingPrefix
+	default:
+		return englishNamingPrefix
+	}
 }
